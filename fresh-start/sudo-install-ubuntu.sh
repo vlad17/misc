7f35448b3ff178cd3d63052c58c43e8c1bc53982
Vladimir Feinberg <vyf@princeton.edu>
2016-10-03 10:25:19 -0400

    get git

--- fresh-start/sudo-install-ubuntu.sh
+++ fresh-start/sudo-install-ubuntu.sh
@@ -9,8 +9,9 @@ sudo apt-get remove -y gnome-mahjongg gnome-mines gnome-sudoku aisleriot
 sudo apt-get remove -y empathy firefox thunderbird rhythmbox 
 sudo apt-get -y remove unity-webapps-common webapp-container
 
-
 cd /tmp
+
+sudo apt-get -y install git
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
