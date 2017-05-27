3d83c545dca3c52b135d44ba8cbe170db27db4ee
Vladimir Feinberg <vladf@berkeley.edu>
2017-05-27 11:13:06 -0400

    removed self-tar file (useless...)

--- fresh-start/tarself.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# A script to tar the fresh-start directory itself.
-# This should be called with the fresh-start directory
-# in the cwd, and generates a file in the cwd.
-
-tar czvf fresh-start.tgz fresh-start/
