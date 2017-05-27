80f5f084af0b11bd91f64e1ed77c731770e5a679
Vladimir Feinberg <vladf@berkeley.edu>
2017-05-26 21:22:20 -0400

    remove python installation from sudo files

--- fresh-start/sudo-install-ubuntu.sh
+++ fresh-start/sudo-install-ubuntu.sh
@@ -16,11 +16,9 @@ sudo apt-get -y install git
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 
-sudo apt-get -y install python3-pip python3-dev build-essential
-sudo pip install --upgrade pip
 sudo apt-get -y install emacs24
 
-sudo apt-get install -y python-dev xclip clang
+sudo apt-get install -y xclip clang
 
 sudo apt-get remove indicator-messages
 sudo add-apt-repository ppa:tsbarnes/indicator-keylock
