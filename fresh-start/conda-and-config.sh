f3cfaa1cd60a3bcebcce4d7a7a7f73814ee6fef1
Vladimir Feinberg <vladf@berkeley.edu>
2017-05-26 21:21:49 -0400

    Added conda installation

--- fresh-start/conda-and-config.sh
+++ fresh-start/conda-and-config.sh
@@ -3,7 +3,8 @@
 # Preps my environment. Does not rely on sudo (downloads, compiles locally).
 # Sets up git config, emacs config, and bash config. Presumes all of these
 # (and hopefully clang) have already been installed.
-
+#
+# Sets up conda, too.
 
 set -e
 set -x
@@ -12,11 +13,19 @@ FRESH_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $FRESH_DIR
 
-printf '# My personal definitions\nsource ~/.bash_defs\n' >> ~/.bashrc
+printf '\n# My bashrc below\n\n' >> ~/.bashrc
+cat .bashrc >> ~/.bashrc
 cp -f .bash_defs ~
 cp -f .tmux.conf ~
 
 cp -rf bin/ ~
+
+cd
+echo "installing conda3"
+wget -O conda.sh --quiet https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+chmod +x conda.sh
+printf "\nqyes\n\nyes\n" | script --return -c "./conda.sh" /dev/null >/dev/null
+rm conda.sh
 
 if ! which git; then
     echo "No git!"
