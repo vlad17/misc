d0d527c8d65c06daf863a5d6d9b0b3f07a762694
Vladimir Feinberg <vladf@berkeley.edu>
2017-06-07 11:16:13 -0400

    weekly body building routine scraping

--- bodybuilding.py
+++ bodybuilding.py
@@ -5,11 +5,16 @@ import requests
 from bs4 import BeautifulSoup
 import itertools
 
-base = 'https://www.bodybuilding.com/fun/kris-gethins-4-weeks-2-shred-day-'
-
+WEEKLY = True
+# base = 'https://www.bodybuilding.com/fun/kris-gethins-4-weeks-2-shred-day-{}'
+base = 'https://www.bodybuilding.com/fun/dtp-week-{}-day-{}.html'
 
 for i in itertools.count(1):
-    r = requests.get(base + str(i))
+    if WEEKLY:
+        url = base.format((i - 1) // 7 + 1, i)
+    else:
+        url = base.format(i)
+    r = requests.get(url)
     soup = BeautifulSoup(r.text, "lxml")
     if soup is None:
         break
@@ -21,9 +26,9 @@ for i in itertools.count(1):
         for j, x in enumerate(xs):
             print('Workout', j, file=f)
             for ex_set in x("div", class_="dpgpt-content"):
-                name = ex_set.find("a").text
+                name = ex_set.find("h4").text
                 rest = ex_set.text
-                rest = rest[len(name):]
+                rest = rest[(rest.find(name) + len(name)):]
                 print('    {} | {}'.format(name, rest), file=f)
     print('fetched workout day', i)
 
