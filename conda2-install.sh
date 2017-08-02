4b36dd86dcbf34ee25be30652e9c5cc1f6e1797a
Vladimir Feinberg <vladf@berkeley.edu>
2017-08-02 09:42:10 -0700

    conda installation bugfix + v2

--- conda2-install.sh
+++ conda2-install.sh
@@ -11,5 +11,5 @@ anacondaURL="https://repo.continuum.io/archive/Anaconda3-4.4.0-Linux-x86_64.sh"
 wget -q "$anacondaURL" -O Anaconda3-4.4.0-Linux-x86_64.sh
 chmod +x Anaconda3-4.4.0-Linux-x86_64.sh
 ./Anaconda3-4.4.0-Linux-x86_64.sh -f -b -p $DEST
-echo "# added by conda-install.sh" >> ~/.bashrc
+echo "# added by conda2-install.sh" >> ~/.bashrc
 echo "export PATH=\"$DEST/bin:\$PATH\"" >> ~/.bashrc
